1dcb2105e97089c1bf50dce2dfae37d314d2aba2
Brian Donovan <me@brian-donovan.com>
2014-10-31 12:05:40 -0700

    Ensure multiple test examples work.

--- test/runner.sh
+++ test/runner.sh
@@ -3,16 +3,18 @@ BROCCOLI=$PWD/node_modules/.bin/broccoli
 ok=true
 
 for example in test/examples/*; do
-  cd ${example}
+  (
+    cd ${example}
 
-  rm -rf output
-  if ${BROCCOLI} build output && node output/output.js; then
-    echo "✓ ${example}"
     rm -rf output
-  else
-    echo "✘ ${example}"
-    ok=false
-  fi
+    if ${BROCCOLI} build output && node output/output.js; then
+      echo "✓ ${example}"
+      rm -rf output
+    else
+      echo "✘ ${example}"
+      ok=false
+    fi
+  )
 done
 
 if ${ok} == "true"; then
